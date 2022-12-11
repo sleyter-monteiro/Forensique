0426eb6cbf4c92226d01fe2e4ace4e851c77f412
Sleyter MONTEIRO <87656234+sleyter92k@users.noreply.github.com>
2022-12-11 17:45:54 +0100

    pdf

---
+++ pdf_meta.py
@@ -0,0 +1,11 @@
+import PyPDF2
+
+
+def get_pdf_meta(file_name):
+    pdf_file = PyPDF2.PdfFileReader(open(file_name, "rb"))
+    doc_info = pdf_file.getDocumentInfo()
+    for info in doc_info:
+        print("[+] " + info + doc_info[info])
+        
+        
+        
